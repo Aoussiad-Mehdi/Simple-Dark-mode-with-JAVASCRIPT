0713951bcf03a5d2d4e8bc6eadf98dc4213690e7
Mehdi Aoussiad <mehdiouss315@gmail.com>
2020-06-16 23:14:48 +0100

    commit1

---
+++ index.js
@@ -0,0 +1,14 @@
+// simple Js .
+function darkMode(){
+    // var declarations.
+    var container = document.querySelector('body');
+    var text = document.querySelector('p');
+    var button = document.querySelector('button');
+  // styling .
+    container.style.background = 'rgb(34, 33, 33)';
+    container.style.transition = '.5s ease';
+    text.style.color = 'white';
+    button.style.cursor = 'pointer';
+    button.style.background = 'crimson';
+    button.style.color = 'white';
+}
